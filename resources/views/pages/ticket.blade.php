f286f9b945a2c7664b0c55a4a8ef9c98ca88065d
Ennio Sousa <ennio@enniosousa.com.br>
2018-09-11 13:35:07 -0300

    Quando for app local aumentar o padding

--- resources/views/pages/ticket.blade.php
+++ resources/views/pages/ticket.blade.php
@@ -4,7 +4,7 @@
 @section('metaDescription', '')
 
 @section('page')
-    <div class="container" style="margin-top: 120px;">
+    <div class="container" style="margin-top: <?= env('APP_ENV', null) === 'local' ? 120 + 60 : 120 ?>px">
         
         @if(\Auth::user()->enrols()->count())
             @include('pages.ticket.update')
